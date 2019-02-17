3b971f964eaf842888babd6da2daf04d0752425e
Naomi Katz <naykatz@gmail.com>
2019-02-17 06:55:43 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -66,6 +69,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+   		  :yukihiro_matsumoto => {
+           :known_for => "Ruby",
+           :languages => ["LISP", "C"]
+   		  },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -102,6 +109,11 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
+     return programmer_hash
+      
      
 end
 
@@ -121,5 +133,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     
+     return programmer_hash
 
 end
