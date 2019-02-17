cc345e9253fcd6e78c1a401be27790a1a8a63e60
Naomi Katz <naykatz@gmail.com>
2019-02-17 06:50:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -66,6 +69,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+   		  :yukihiro_matsumoto => {
+           :known_for => "Ruby",
+           :languages => ["LISP", "C"]
+   		  },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
